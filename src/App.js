4c4e81872d47db41959259ce56e2334e99e791d9
Gaston Martinez <gastonmartinezeq@gmail.com>
2022-08-28 18:49:15 -0300

    primera entrega

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gaston Martinez
         </p>
+        
+        <p>Hudson 56</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
